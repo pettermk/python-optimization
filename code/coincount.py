4ba72152cee949c80e859a6b818d3283e8607117
Petter Moe Kvalvaag <pmkv@equinor.com>
2020-03-01 22:28:44 +0100

    Some cleanup

--- code/coincount.py
+++ code/coincount.py
@@ -21,7 +21,7 @@ def circlecost(x, inputimage):
     #result = circlecost_helper(x, inputimage)
     # print(type(inputimage[0][0]))
     # print(inputimage.ndim)
-    return cost_function(x, inputimage)
+    # return cost_function(x, inputimage)
     innervalue, outervalue = circlecost_helper(x, inputimage)
     if innervalue==0:
         return 2
@@ -38,14 +38,10 @@ def circlecost_helper(x, inputimage):
     size_x = len(inputimage)
     size_y = len(inputimage[0])
     x = [x[0]*size_x, x[1]*size_y, x[2]*max(size_x, size_y)]
-    outersum = 0
-    outercount = 0
-    innersum = 0
-    innercount = 0
-    #if x[0] < x[2] or x[0] > size_x- x[2]:
-    #    return 0, 1000
-    #if x[1] < x[2] or x[1] > size_y - x[2]:
-    #    return 0, 1000
+    outersum = 0.
+    outercount = 0.
+    innersum = 0.
+    innercount = 0.
     j_pow = []
     for k in range(floor(x[1] - x[2]), floor(x[1] + x[2]), 1):
         j_pow.append(pow(k - x[1], 2))
@@ -121,61 +117,27 @@ def drawcircle(inputimage, x, y, radius, new_value=30):
     outputimage[index]=new_value
     return outputimage
 
-def main():
+def count_coins(threshold: float, max_no_coins: int = 100, optimizer: str = 'fmin', costfunc=circlecost):
     coins = imread('Images/coins.png')
+    optimizers = {
+        'diff_evo': lambda x: differential_evolution(x, bounds=[(0,1), (0,1), (0.01,0.2)], args=(coins,), seed=1),
+        'fmin': lambda x: minimize(x, [0.4, 0.7, 0.1], method='Nelder-Mead', args=(coins,))
+    }
     cost = 0
     found_coins = -1
-    while cost < 0.4 and found_coins < 9:
-        #print(coins)
-        # x_opt = fmin(circlecost, [0.4, 0.7, 0.1], args=(coins,))
-        x_opt = differential_evolution(circlecost, bounds=[(0,1), (0,1), (0.01,0.2)], args=(coins,))
-                                        #minimizer_kwargs={'args':(coins,),
-                                        #                  'method': 'Nelder-Mead'})
-        # x_opt = minimize(circlecost, [0.5, 0.5, 0.1], args=(coins,),
-        #                  method='COBYLA', bounds=[(0,1), (0,1), (0.01,0.2)],
-        #                  options={'rhobeg':[0.1, 0.1, 0.05]})
+    while cost < threshold and found_coins < max_no_coins:
+        x_opt = optimizers[optimizer](circlecost)
         print(x_opt)
         coins = drawcircle(coins, *(x_opt.x))
         cost = x_opt.fun
         found_coins = found_coins + 1
     print('Found {} coins'.format(found_coins))
     imshow(coins)
-    #print(x_opt)
-    #x_opt = fmin(circlecost, [0.2, 0.2, 0.1], args=(coins2,))
-    #coins3 = drawcircle(coins2, x_opt[0], x_opt[1], x_opt[2])
-    #x_opt = fmin(circlecost, [0.3, 0.8, 0.1], args=(coins3,)) 
-    #coins4 = drawcircle(coins3, x_opt[0], x_opt[1], x_opt[2])
-    #x_opt = fmin(circlecost, [0.8, 0.8, 0.1], args=(coins4,)) 
-    #coins5 = drawcircle(coins4, x_opt[0], x_opt[1], x_opt[2])
-    #x_opt = fmin(circlecost, [0.7, 0.4, 0.1], args=(coins5,)) 
-    #coins6 = drawcircle(coins5, x_opt[0], x_opt[1], x_opt[2])
-    #x_opt = fmin(circlecost, [0.7, 0.4, 0.1], args=(coins6,)) 
-    #coins7 = drawcircle(coins6, x_opt[0], x_opt[1], x_opt[2])
-    #x_opt = fmin(circlecost, [0.7, 0.4, 0.1], args=(coins7,)) 
-    #coins8 = drawcircle(coins7, x_opt[0], x_opt[1], x_opt[2])
-    #minimizer_kwargs = {'method' : 'Nelder-Mead', 'args' : (coins7,)}
-    #result = dual_annealing(circlecost, [(0.,1.), (0.,1.), (0.,40.)], minimizer_kwargs=minimizer_kwargs)
-    #x_opt = result.x
-    #coins8 = drawcircle(coins7, x_opt[0], x_opt[1], x_opt[2])
-    #minimizer_kwargs = {'method' : 'Nelder-Mead', 'args' : (coins8,)}
-    #result = basinhopping(circlecost, [0.4, 0.7, 0.1], minimizer_kwargs=minimizer_kwargs)
-    #x_opt = result.x
-    #coins9 = drawcircle(coins8, x_opt[0], x_opt[1], x_opt[2])
-    #minimizer_kwargs = {'method' : 'Nelder-Mead', 'args' : (coins9,)}
-    #result = basinhopping(circlecost, [0.4, 0.7, 0.1], minimizer_kwargs=minimizer_kwargs)
-    #x_opt = result.x
-    #coins10 = drawcircle(coins9, x_opt[0], x_opt[1], x_opt[2])
-    #minimizer_kwargs = {'method' : 'Powell', 'args' : (coins10,)}
-    #result = basinhopping(circlecost, [0.4, 0.7, 0.1], minimizer_kwargs=minimizer_kwargs)
-    #x_opt = result.x
-    #coins11 = drawcircle(coins10, x_opt[0], x_opt[1], x_opt[2])
-    # imshow(coins2)
-
         
 if __name__ == "__main__":
     import sys
     t = time.process_time()
-    main()
+    count_coins(threshold=0.1, max_no_coins=100, optimizer='diff_evo')
     elapsed_time = time.process_time() - t
     print("Took %s seconds" % elapsed_time)
 
