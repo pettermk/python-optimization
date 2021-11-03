7619c39c82815655eb4074da6acb5622be132f52
Petter Kvalvaag <pmkv@equinor.com>
2021-11-03 11:30:49 +0100

    Cleaned up coincount file

--- code/coincount.py
+++ code/coincount.py
@@ -7,33 +7,26 @@ Created on Mon Nov 17 20:27:40 2014
 
 import time
 
-from find_coin import cost_function
 import numpy as np
 from imageio import imread
 from matplotlib.pyplot import imshow
 from scipy.optimize import fmin, fmin_cg, fmin_bfgs, differential_evolution, basinhopping, minimize
-from numba import jit, int32, int8, float64
+from numba import jit, njit, int32, int8, float64, prange
 
 from math import sqrt, floor
 
+USE_COMPILED = False
 
 def circlecost(x, inputimage):
-    #result = circlecost_helper(x, inputimage)
-    # print(type(inputimage[0][0]))
-    # print(inputimage.ndim)
-    # return cost_function(x, inputimage)
+    if USE_COMPILED:
+        from find_coin import cost_function
+        return cost_function(x, inputimage)
+    
     innervalue, outervalue = circlecost_helper(x, inputimage)
     if innervalue==0:
         return 2
-    #return -(innervalue - outervalue)
     return outervalue / innervalue
-    
-def circlecost_outermean(x, inputimage):
-    #innermean, outermean = circlecost_helper(x, inputimage)
-    return outermean    
 
-#@jit(float64(int32[:], int8[:,:]))
-@jit
 def circlecost_helper(x, inputimage):
     size_x = len(inputimage)
     size_y = len(inputimage[0])
@@ -55,8 +48,6 @@ def circlecost_helper(x, inputimage):
             if j >= size_y:
                 continue
             local_radius = sqrt(i_squared + j_pow[n])
-            #if local_radius > x[2]:
-            #    continue
             n +=1
             if local_radius > x[2]:
                 outersum += pow(inputimage[i][j],2)
@@ -64,8 +55,6 @@ def circlecost_helper(x, inputimage):
             if local_radius <=  x[2]:
                 innersum += pow(inputimage[i][j],2)
                 innercount += 1
-    #if outercount == 0 or innercount == 0:
-    #    return 0, 1000
         
     return innersum/innercount, outersum/outercount
     
@@ -82,30 +71,6 @@ def circlecost_gradient(x, *inputimage):
     
     return [deltax / df_x, deltay /df_y, deltar / df_r]
     
-def totuple(a):
-    try:
-        return tuple(totuple(i) for i in a)
-    except TypeError:
-        return a
-        
-def removecoin(inputimage, startguess=[0.5, 0.5, 0.1], method='local'):
-    if method == 'local':
-        x_opt = fmin(circlecost, [0.4, 0.7, 0.1], args=(inputimage))
-        print(x_opt)
-        outermean = circlecost_outermean(x_opt, totuple(inputimage))
-        outputimage = drawcircle(inputimage, x_opt[0], x_opt[1], x_opt[2], 0)
-
-    if method == 'global':
-        minimizer_kwargs = {'method' : 'Nelder-Mead', 'args' : totuple(inputimage)}
-        result = basinhopping(circlecost, [0.4, 0.7, 0.1], 
-                              minimizer_kwargs=minimizer_kwargs)
-        x_opt = result.x
-        print(x_opt)
-        outermean = circlecost_outermean(x_opt, totuple(inputimage))
-        outputimage = drawcircle(inputimage, x_opt[0], x_opt[1], x_opt[2], 0)
-        
-    imshow(outputimage)        
-    return outputimage
 
 def drawcircle(inputimage, x, y, radius, new_value=30):
     outputimage = inputimage.copy()
@@ -121,15 +86,17 @@ def count_coins(threshold: float, max_no_coins: int = 100, optimizer: str = 'fmi
     coins = imread('Images/coins.png')
     optimizers = {
         'diff_evo': lambda x: differential_evolution(x, bounds=[(0,1), (0,1), (0.01,0.2)], args=(coins,), seed=1),
-        'fmin': lambda x: minimize(x, [0.4, 0.7, 0.1], method='Nelder-Mead', args=(coins,))
+        'fmin': lambda x: minimize(x, [0.4, 0.6, 0.1], method='Nelder-Mead', args=(coins,))
     }
     cost = 0
-    found_coins = -1
+    found_coins = 0
     while cost < threshold and found_coins < max_no_coins:
         x_opt = optimizers[optimizer](circlecost)
         print(x_opt)
-        coins = drawcircle(coins, *(x_opt.x))
         cost = x_opt.fun
+        if cost >= threshold:
+            continue
+        coins = drawcircle(coins, *(x_opt.x))
         found_coins = found_coins + 1
     print('Found {} coins'.format(found_coins))
     imshow(coins)
@@ -137,7 +104,8 @@ def count_coins(threshold: float, max_no_coins: int = 100, optimizer: str = 'fmi
 if __name__ == "__main__":
     import sys
     t = time.process_time()
-    count_coins(threshold=0.1, max_no_coins=100, optimizer='diff_evo')
+    USE_COMPILED=False
+    count_coins(threshold=0.11, max_no_coins=1, optimizer='fmin')
     elapsed_time = time.process_time() - t
     print("Took %s seconds" % elapsed_time)
 
